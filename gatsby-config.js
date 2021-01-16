3b76762b8f1b87960313a433ed8a5868fe21f214
marcelin <margaal@pop-os.localdomain>
2021-01-16 07:38:31 +0100

    Netlify cms has done

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@ module.exports = {
         `gatsby-source-data`,
         `gatsby-transformer-remark`,
         {
+            resolve: `gatsby-plugin-netlify-cms`,
+            options: {
+                modulePath: `${__dirname}/src/cms/cms.js`
+            }
+        },
+        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `pages`,
